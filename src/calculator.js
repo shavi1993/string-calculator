50f1fae58ee5ce225b7f6b08e3651032053a3a93
shavi1993 <rishishavi@gmail.com>
2025-02-28 12:04:32 +0800

    sum of two number using comma

--- src/calculator.js
+++ src/calculator.js
@@ -1,7 +1,9 @@
 function add(numbers) {
     if (numbers === "") return 0;
-    return parseInt(numbers);  // Parse the number and return
-  }
-  
-  module.exports = { add };
-  
+    // return parseInt(numbers);  // Parse the number and return
+    const numArray = numbers.split(',').map(num => parseInt(num));
+    return numArray.reduce((acc, num) => acc + num, 0); // Sum the numbers
+
+}
+
+module.exports = { add };
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,10 +1,14 @@
 const { add } = require('../src/calculator');
 
 test('should return 0 for an empty string', () => {
-  expect(add("")).toBe(0);
+    expect(add("")).toBe(0);
 });
 
 test('should return the same number when the input is a single number', () => {
     expect(add("1")).toBe(1);
-  });
-  
+});
+
+test('should return the sum of two numbers', () => {
+    expect(add("2,4")).toBe(6);
+});
+ 
