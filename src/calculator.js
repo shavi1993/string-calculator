ad94d1b802b596cafa4f7f2fd2feddb42c9316c7
shavi1993 <rishishavi@gmail.com>
2025-02-28 11:56:12 +0800

    Add test for empty string input

---
+++ src/calculator.js
@@ -0,0 +1,6 @@
+function add(numbers) {
+    if (numbers === "") return 0;
+  }
+  
+  module.exports = { add };
+  
---
+++ tests/calculator.test.js
@@ -0,0 +1,6 @@
+const { add } = require('../src/calculator');
+
+test('should return 0 for an empty string', () => {
+  expect(add("")).toBe(0);
+});
+
