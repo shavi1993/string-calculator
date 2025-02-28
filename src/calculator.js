b7a4ec32c40a8661179f00f604d0367da2659f5f
shavi1993 <rishishavi@gmail.com>
2025-02-28 12:06:43 +0800

    sum of two number using delimiter

--- src/calculator.js
+++ src/calculator.js
@@ -1,8 +1,11 @@
 function add(numbers) {
     if (numbers === "") return 0;
     // return parseInt(numbers);  // Parse the number and return
-    const numArray = numbers.split(',').map(num => parseInt(num));
-    return numArray.reduce((acc, num) => acc + num, 0); // Sum the numbers
+    // const numArray = numbers.split(',').map(num => parseInt(num));
+    // return numArray.reduce((acc, num) => acc + num, 0); // Sum the numbers
+
+    const numArray = numbers.split(/[\n,]/).map(num => parseInt(num));
+    return numArray.reduce((acc, num) => acc + num, 0); // sum using delimiter
 
 }
 
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -11,4 +11,7 @@ test('should return the same number when the input is a single number', () => {
 test('should return the sum of two numbers', () => {
     expect(add("2,4")).toBe(6);
 });
- 
+
+test('should handle new line separators between numbers', () => {
+    expect(add("1\n2,3")).toBe(6);
+});
