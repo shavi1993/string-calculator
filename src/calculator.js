d431c9fd5522b040f7ed2c64c06736b98c140cd5
shavi1993 <rishishavi@gmail.com>
2025-02-28 12:17:10 +0800

    handle custom delimiters

--- src/calculator.js
+++ src/calculator.js
@@ -1,11 +1,22 @@
 function add(numbers) {
     if (numbers === "") return 0;
     // return parseInt(numbers);  // Parse the number and return
+
     // const numArray = numbers.split(',').map(num => parseInt(num));
     // return numArray.reduce((acc, num) => acc + num, 0); // Sum the numbers
 
-    const numArray = numbers.split(/[\n,]/).map(num => parseInt(num));
-    return numArray.reduce((acc, num) => acc + num, 0); // sum using delimiter
+    // const numArray = numbers.split(/[\n,]/).map(num => parseInt(num));
+    // return numArray.reduce((acc, num) => acc + num, 0); // sum using delimiter
+
+    let delimiter = ",";
+    if (numbers.startsWith("//")) {
+        const delimiterLine = numbers.split("\n")[0];
+        delimiter = delimiterLine.slice(2);  // Extract the custom delimiter
+        numbers = numbers.slice(delimiterLine.length + 1);  // Remove the delimiter definition line
+    }
+
+    const numArray = numbers.split(new RegExp(`[${delimiter}\n]`)).map(num => parseInt(num));
+    return numArray.reduce((acc, num) => acc + num, 0);
 
 }
 
--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -15,3 +15,7 @@ test('should return the sum of two numbers', () => {
 test('should handle new line separators between numbers', () => {
     expect(add("1\n2,3")).toBe(6);
 });
+
+test('should handle custom delimiters', () => {
+    expect(add("//;\n1;2")).toBe(3);
+});
